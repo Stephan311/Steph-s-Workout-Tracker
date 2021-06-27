974e5c66863ea7c6b4ef075b29ddc1a18a2a5a5f
stephan du Plooy <sdpphotography1@gmail.com>
2021-06-26 22:19:04 -0400

    added routes for all pages

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.get("/excersise", (req, res) => {
-    res.sendFile(path.join(__dirname + "/public/index.html"));
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
 });
 
 app.get("/all", (req, res) => {
